255dc7e98dd14a885661c6b88151bb2c4f8eb6ed
UnluckyNinja <UnluckyNinja@users.noreply.github.com>
2019-06-16 02:24:30 +0800

    github page build

---
+++ precache-manifest.113314f78ce03e01e062b71ad8775316.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "fb8fcf74e5005f654a16",
+    "url": "/js/chunk-vendors.991d1999.js"
+  },
+  {
+    "revision": "3adb40cee5072d8773b8",
+    "url": "/js/app.e8163e42.js"
+  },
+  {
+    "revision": "5af5a6695077ba6e36400079cca0c5a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bea19207b096dcb0a10cc0bd07fa25b",
+    "url": "/img/cyberpunk_zh.2bea1920.png"
+  },
+  {
+    "revision": "2827521553fc33d4d0dba4f7d471bb35",
+    "url": "/img/cyberpunk_en.28275215.png"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "3adb40cee5072d8773b8",
+    "url": "/css/app.8334e50a.css"
+  }
+];
---
+++ precache-manifest.3e928edf9ab37e3804280abf07762683.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "298c301ee70b91b046f9",
+    "url": "/js/chunk-vendors-legacy.e3c3d6d2.js"
+  },
+  {
+    "revision": "b3b15dd16eecf61145a3",
+    "url": "/js/app-legacy.e0bc54fc.js"
+  },
+  {
+    "revision": "60a8025cf28a7a9eb5b1d59f61488935",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bea19207b096dcb0a10cc0bd07fa25b",
+    "url": "/img/cyberpunk_zh.2bea1920.png"
+  },
+  {
+    "revision": "2827521553fc33d4d0dba4f7d471bb35",
+    "url": "/img/cyberpunk_en.28275215.png"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "b3b15dd16eecf61145a3",
+    "url": "/css/app.8334e50a.css"
+  }
+];
