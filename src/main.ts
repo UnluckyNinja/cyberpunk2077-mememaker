1509137b84ee34a3b7e0bb6c53dd73b82467cc4f
UnluckyNinja <UnluckyNinja@users.noreply.github.com>
2019-06-16 23:53:32 +0800

    add vi18n

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import store from './store';
 // import './registerServiceWorker';
 import './plugins/element.js';
 import VueKonva from 'vue-konva';
+import i18n from './i18n';
 
 Vue.use(VueKonva);
 
@@ -11,5 +12,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   store,
+  i18n,
   render: (h) => h(App),
 }).$mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   publicPath: '/cyberpunk2077-mememaker/',
+
   configureWebpack:{
     devtool: 'source-map'
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'zh-cn',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true
+    }
   }
 }
